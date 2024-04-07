10cc5b922be66ba703d40dc0165c5faec701c4f6
Kseniia Andreeva <41472942+ksunandr@users.noreply.github.com>
2024-04-07 20:35:34 +0100

    Update addPicturiesToSlides.js

--- addPicturiesToSlides.js
+++ addPicturiesToSlides.js
@@ -10,7 +10,6 @@ var folders = {
   small: DriveApp.getFolderById('18JFnTh89XY3g-3OH1ynexOTrSa9OX9au'), // small images
   middle: DriveApp.getFolderById('1VGTR9908WUTcS9tCaCLU_tWiJfN1hO1A') // middle images
 };
-console.log(1); // Indicate that slides and files are ready for processing
 
 // Simplify file retrieval with iterators
 var iterators = {
@@ -21,8 +20,6 @@ var iterators = {
 
 
 
-  console.log(2); // Indicate that slides and files are ready for processing
-
   slides.forEach(function(slide, index) {
     console.log("Processing slide #" + (index + 1));
     var randomizer = index % 2 === 0 ? 2 : 1;
@@ -30,45 +27,52 @@ var iterators = {
     slide.getShapes().forEach(function(shape) {
       if (shape.getShapeType() === SlidesApp.ShapeType.TEXT_BOX) {
         var text = shape.getText().asString();
-        console.log(text); // Log the text found
+
         if (text.includes('pic')) {
-        var fileType = '';
-        if (text.includes('{pic}')) fileType = 'big';
-        else if (text.includes(`{pic${randomizer}}`)) fileType = 'big';
-        else if (text.includes(`{midpic${randomizer}}`)) fileType = 'middle';
-        else if (text.includes(`{smallpic${randomizer}}`)) fileType = 'small';
+          var fileType = '';
+          if (text.includes('{pic}')) fileType = 'big';
+          else if (text.includes(`{pic${randomizer}}`)) fileType = 'big';
+          else if (text.includes(`{midpic${randomizer}}`)) fileType = 'middle';
+          else if (text.includes(`{smallpic${randomizer}}`)) fileType = 'small';
 
-        if (fileType && iterators[fileType].hasNext()) {
-          var file = iterators[fileType].next();
-          
-          console.log("Processing file " + file.getName() +" " + fileType);
-          
-          var size;
+          if (fileType && iterators[fileType].hasNext()) {
+            var file = iterators[fileType].next();
+            
+            
+            var size;
 
-          if (fileType === 'small') {
-            size = { width: 25, height: 35 };
-          } else if (fileType === 'middle') {
-            size = { width: 45, height: 55 };
-          } else {
-            size = { width: 70, height: 100 };
+            if (fileType === 'small') {
+              size = { width: 25, height: 35 };
+            } else if (fileType === 'middle') {
+              size = { width: 45, height: 55 };
+            } else {
+              size = { width: 70, height: 100 };
+            }
+
+
+            addPicToSlide(file, slide, shape, size.width, size.height);
           }
-
-
-          addPicToSlide(file, slide, shape, size.width, size.height);
-        }
-        
+          
           shape.remove(); // Remove the placeholder text box
         }
       }
     });
   });
 
-  console.log(8); // Indicate script completion
+  console.log("end " +countItemsInIterator(iterators['big'])); // Indicate script completion
+}
+
+function countItemsInIterator(iterator) {
+  var count = 0;
+  while (iterator.hasNext()) {
+    iterator.next();
+    count++;
+  }
+  return count;
 }
 
 function addPicToSlide(file, slide, shape, maxWidth, maxHeight) {
   var imageUrl = "https://drive.google.com/uc?export=view&id=" + file.getId();
-  console.log("Inserting image from URL: " + imageUrl);
   var image = slide.insertImage(imageUrl);
 
   var imageWidth = image.getWidth();
@@ -88,5 +92,4 @@ function addPicToSlide(file, slide, shape, maxWidth, maxHeight) {
   image.setLeft(centerX);
   image.setTop(centerY);
 
-  console.log(7); // Indicate that the image has been positioned and sized
 }
